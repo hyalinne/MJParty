fb7396cfdc8812ca05d7ea8c0b77695dbb6cf8f5
hyalinne <gus2294@gmail.com>
2016-11-15 18:47:02 +0900

    11.15 add Login

--- app/src/main/java/com/example/hyalinne/mjparty/MainActivity.java
+++ app/src/main/java/com/example/hyalinne/mjparty/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.hyalinne.mjparty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isLogin;
+
+    public MainActivity() {
+        isLogin = false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(!isLogin) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     public void findParty(View view) {
